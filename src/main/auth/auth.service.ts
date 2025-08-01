965fd38bb3b4deb37a67068b04eb89464d129753
linux44gh6 <abireshan32@gmail.com>
2025-08-01 23:57:32 +0600

    addAuth

--- src/main/auth/auth.service.ts
+++ src/main/auth/auth.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createAuthDto: CreateAuthDto, profileDTO: any) {
+    return this.prisma.$transaction(async (tx) => {
+      const {  password} = createAuthDto;
+      const hasshedPassword = await bcrypt.hash(password, 10);
+      // Step 1: Create credential using createAuthDto
+      const credential = await tx.credential.create({
+        data: {
+          email: createAuthDto.email,
+          password: hasshedPassword,
+          fullName: createAuthDto.fullName,
+          userName: createAuthDto.userName,
+        },
+      });
+
+      // Step 2: Create user profile using profileDTO and link it to credential
+      const userProfile = await tx.userProfile.create({
+        data: {
+          ...profileDTO, 
+          userId: credential.id, 
+        },
+      });
+
+      return {
+        credential,
+        userProfile,
+      };
+    });
   }
 
   findAll() {
