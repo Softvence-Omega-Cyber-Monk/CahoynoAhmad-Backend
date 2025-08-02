4885181aba1770d336d3b39a4990e0aa8fa66833
linux44gh6 <abireshan32@gmail.com>
2025-08-02 23:19:37 +0600

    setup messaing system

--- src/main/contact/contact.service.ts
+++ src/main/contact/contact.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { MailService } from '../mail/mail.service';
 
 @Injectable()
 export class ContactService {
-  create(createContactDto: CreateContactDto) {
-    return 'This action adds a new contact';
+  constructor(private readonly mailService: MailService) {}
+ async sendMessage(createContactDto: CreateContactDto) {
+   try{
+      const result=await this.mailService.sendMail({
+        to:process.env.SMTP_USER as string,
+        subject:'Contact Message',
+        html:`<h1>Name: ${createContactDto.name}</h1><h1>Email: ${createContactDto.email}</h1><h2>Message: ${createContactDto.message}</h2>`,
+        from:process.env.SMTP_USER as string,
+      });
+   }catch(err){
+      throw new Error('Failed to create contact');
+    }
   }
 
   findAll() {
