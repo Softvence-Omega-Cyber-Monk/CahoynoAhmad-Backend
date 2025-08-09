558e4b4baa97edb886bff6de40414efca54ea3aa
linux44gh6 <abireshan32@gmail.com>
2025-08-10 00:36:23 +0600

    add data

--- src/main/strip-payment/strip-payment.service.ts
+++ src/main/strip-payment/strip-payment.service.ts
@@ -5,7 +5,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class StripeService {
-    private stripe: Stripe; // Declare a private stripe instance
+    private stripe: Stripe; 
 
     constructor(private readonly configService: ConfigService,private prisma:PrismaService) {
         const stripeSecretKey = this.configService.get<string>('STRIPE_SECRET_KEY');
