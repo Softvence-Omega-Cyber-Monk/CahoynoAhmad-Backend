1b763fbddfed4130a3d275fe8578f20d35f95cb7
linux44gh6 <abireshan32@gmail.com>
2025-08-18 12:23:45 +0600

    add data

--- src/main/ai/ai.service.ts
+++ src/main/ai/ai.service.ts
@@ -5,27 +5,33 @@ import { UpdateAiDto } from './dto/update-ai.dto';
 @Injectable()
 export class AiService {
   async create(createAiDto: CreateAiDto) {
-    const { mode, promt,session_id,user_plan } = createAiDto;
+    const { text,tone,session_id,user_plan } = createAiDto;
+    console.log(createAiDto);
+    
     try {
       const response = await fetch(`${process.env.AI_URL}/rewrite`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({ tone: mode, text: promt, session_id,user_plan}),
+        body: JSON.stringify({ tone, text, session_id,user_plan}),
       });
+      console.log(response);
+      
       const data = await response.json();
+      console.log(data);
+      
       return data;
     } catch (err) {
       throw new HttpException(err.message, 500);
     }
   }
 
-  async getAllHistory(session_id) {
+  async getAllHistory(user) {
     
     try{
       const response = fetch(`${process.env.AI_URL}/memory`, {
         method: 'POST',
         headers:{'Content-Type': 'application/json'},
-        body:JSON.stringify({session_id})
+        body:JSON.stringify({session_id:user.userId})
       })
       const data=await (await response).json();
       return data;
