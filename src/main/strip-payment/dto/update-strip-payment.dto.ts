fb7957c869a347630d6c0c515223a135ff167daa
linux44gh6 <abireshan32@gmail.com>
2025-08-28 09:24:06 +0600

    add data

--- src/main/strip-payment/dto/update-strip-payment.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStripePaymentDto } from './create-strip-payment.dto';
-
-export class UpdateStripPaymentDto extends PartialType(
-  CreateStripePaymentDto,
-) {}
