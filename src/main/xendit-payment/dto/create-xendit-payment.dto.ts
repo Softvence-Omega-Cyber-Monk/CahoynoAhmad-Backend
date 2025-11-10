5ebf58ed00844737fdfb5caf6c62f537f4e48438
linux44gh6 <abireshan32@gmail.com>
2025-11-10 10:35:57 +0600

    complet xendit payment method

--- src/main/xendit-payment/dto/create-xendit-payment.dto.ts
+++ src/main/xendit-payment/dto/create-xendit-payment.dto.ts
@@ -1,21 +1,17 @@
 // src/main/xendit-payment/dto/create-xendit-payment.dto.ts
 
-import { 
-  IsNotEmpty, 
-  IsEmail, 
-  IsNumber, 
-  IsPositive, 
-  IsString, 
+import {
+  IsNotEmpty,
+  IsEmail,
+  IsNumber,
+  IsPositive,
+  IsString,
   Min,
-  IsOptional
+  IsOptional,
 } from 'class-validator';
-import { 
-  ApiProperty, 
-  ApiPropertyOptional
-} from '@nestjs/swagger'; 
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateXenditPaymentDto {
-  
   @ApiProperty({
     description: 'The total amount to be charged for the payment.',
     example: 50000,
@@ -24,7 +20,7 @@ export class CreateXenditPaymentDto {
   @IsNotEmpty({ message: 'Amount is required.' })
   @IsNumber({}, { message: 'Amount must be a valid number.' })
   @IsPositive({ message: 'Amount must be a positive value.' })
-  @Min(1000, { message: 'Minimum amount for this currency is 1000 (e.g., IDR 1,000).' }) 
+  @Min(1000, { message: 'Minimum amount for this currency is 1000 (e.g., IDR 1,000).' })
   amount: number;
 
   @ApiProperty({
@@ -42,7 +38,7 @@ export class CreateXenditPaymentDto {
   @IsNotEmpty({ message: 'Item description is required.' })
   @IsString({ message: 'Description must be a string.' })
   description: string;
-  
+
   @ApiPropertyOptional({
     description: 'The currency code of the invoice.',
     example: 'IDR',
@@ -50,14 +46,30 @@ export class CreateXenditPaymentDto {
   })
   @IsOptional()
   @IsString({ message: 'Currency must be a string.' })
-  currency?: string = 'IDR'; 
-  
-  @ApiPropertyOptional({
-    description: 'Optional custom data to be passed and returned in the webhook.',
-    example: '{ "userId": 12345 }',
-    type: String, 
-  })
-  @IsOptional() 
-  @IsString({ message: 'Custom metadata must be a string.' })
-  metadata?: string; 
+  currency?: string = 'IDR';
+
+  // @ApiPropertyOptional({
+  //   description: 'Optional custom data to be passed and returned in the webhook.',
+  //   example: '{ "userId": 12345 }',
+  //   type: String,
+  // })
+  // @IsOptional()
+  // @IsString({ message: 'Custom metadata must be a string.' })
+  // metadata?: string;
+
+  // @ApiProperty({
+  //   description: 'The ID of the plan associated with this payment.',
+  //   example: 'plan_123',
+  // })
+  // @IsNotEmpty({ message: 'Plan ID is required.' })
+  // @IsString({ message: 'Plan ID must be a string.' })
+  // planId: string;
+
+  // @ApiProperty({
+  //   description: 'The name of the plan associated with this payment.',
+  //   example: 'Premium Plan',
+  // })
+  // @IsNotEmpty({ message: 'Plan name is required.' })
+  // @IsString({ message: 'Plan name must be a string.' })
+  // planName: string;
 }
