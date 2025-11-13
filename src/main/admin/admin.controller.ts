c62ea6ad53f29b31391e96dfc24941593b351852
linux44gh6 <abireshan32@gmail.com>
2025-11-14 04:35:01 +0600

    udpate user profile

--- src/main/admin/admin.controller.ts
+++ src/main/admin/admin.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException, Query, UseGuards, Req, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { getUserDTO } from './dto/get_all_user';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/utils/jwt-auth.guard';
 import { RolesGuard } from 'src/utils/authorization/roles.guard';
 import { Roles } from 'src/utils/authorization/roles.decorator';
 import { Role } from 'src/utils/authorization/role.enum';
 import { GetAllPaymentDto } from './dto/get.payment.dto';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -50,6 +52,52 @@ export class AdminController {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
     }
   }
+
+  @Patch('update-profile')
+@ApiConsumes('multipart/form-data') 
+@UseGuards(JwtAuthGuard,RolesGuard)
+  @ApiBearerAuth()
+  @Roles(Role.Admin)
+@ApiOperation({ summary: 'Update admin Profile' })
+@ApiBody({
+  schema: {
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      phone: { type: 'string' },
+      file: { type: 'string', format: 'binary' },
+    },
+  },
+})
+@UseInterceptors(
+  FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `quran-${uniqueSuffix}${extname(file.originalname)}`);
+      },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  }),
+)
+async updateAdminProfile(
+  @Req() req: any,
+  @Body() updateAdminDto: any,
+  @UploadedFile() file: Express.Multer.File,
+) {
+  try {
+    const id = req.user.userId;
+    const res = await this.adminService.updateAdminProfile(id, updateAdminDto, file);
+    return {
+      status: HttpStatus.ACCEPTED,
+      message: 'Admin profile updated successfully',
+      data: res,
+    };
+  } catch (error) {
+    throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+  }
+}
   // get all user by admin
   @Get('user')
   @ApiOperation({summary:"get all user and manage search"})
@@ -126,4 +174,8 @@ export class AdminController {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
     }
   }
+
+
+ 
+
 }
